11fbdce2d59c4e2f9831262e564615f261269343
leo F <leo@catsconsultants.com>
2023-10-03 10:45:11 +0000

    simple counter

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ struct Square {
     y: f32,
     size: f32,
     color: Color,
+    active: bool,
+    number: u8
 }
 
 impl Square {
-    fn new(x: f32, y: f32, size: f32, color: Color) -> Square {
-        Square { x, y, size, color }
+    fn new(x: f32, y: f32, size: f32, color: Color, active: bool, number: u8) -> Square {
+        Square { x, y, size, color, active, number }
     }
 
     fn draw(&self, font: &Font) {
         draw_rectangle(self.x, self.y, self.size, self.size, self.color);
+        if self.active {
         draw_text_ex(
-            "1",
-            self.x + self.size/3.75,
-            self.y - self.size/2.75,
+            &self.number.to_string(),
+            self.x + self.size*0.25,
+            self.y + self.size*0.8,
             TextParams {
                 font: Some(font),
                 font_size: 24,
@@ -27,6 +30,7 @@ impl Square {
                 color: BLACK,
             },
         );
+        }
     }
 
     fn is_mouse_over(&self) -> bool {
@@ -45,13 +49,14 @@ async fn main() {
     let grid_size = 10;
     let start_x = 100.0;
     let start_y = 100.0;
+    let mut counter: u8 = 0;
 
     // Initialize the squares grid
     for i in 0..grid_size {
         for j in 0..grid_size {
             let x = start_x + j as f32 * (square_size + gap);
             let y = start_y + i as f32 * (square_size + gap);
-            squares.push(Square::new(x, y, square_size, WHITE));
+            squares.push(Square::new(x, y, square_size, WHITE, false, 0));
         }
     }
 
@@ -60,8 +65,15 @@ async fn main() {
 
         for square in squares.iter_mut() {
             square.draw(&font);
-            if is_mouse_button_down(MouseButton::Left) && square.is_mouse_over() {
+            if is_mouse_button_pressed(MouseButton::Left) && square.is_mouse_over() && square.active == false  {
                 square.color = RED;
+                square.active = true;
+                square.number = counter;
+                if counter < 9 {
+                    counter += 1;
+                } else {
+                    counter = 0;
+                }
             }
         }
 
